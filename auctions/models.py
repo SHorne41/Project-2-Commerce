8f934b01b660eaf6e363a9bfddb3ecd08232c754
SHorne41 <stephane.horne@gmail.com>
2020-07-31 17:14:18 -0400

    Added models for Bids, Listings, and Comments

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,15 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+
+class Bids(models.Model):
+    bid = models.IntegerField()
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+
+class Comments(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    comment = models.TextField()
