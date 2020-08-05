7d7532317805dcbbc6822f33eaddd7619a1f6f35
SHorne41 <stephane.horne@gmail.com>
2020-08-04 21:48:28 -0400

    Solved migrations issue

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,17 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    category = models.CharField(max_length=64)
+    image = models.CharField(max_length=100)
+    bid = models.IntegerField()
+
+class Watchlist(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+
+class Comments(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    comment = models.TextField()
