5d6e585024bc3102219a92b4621065e5a8708f9d
SHorne41 <stephane.horne@gmail.com>
2020-08-06 09:47:10 -0400

    URL path added for listing_view

--- auctions/urls.py
+++ auctions/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("newListing", views.new_listing, name="new"),
-    path("createListing", views.create_listing, name="create")
+    path("createListing", views.create_listing, name="create"),
+    path("<str:title>Listing", views.listing_view, name="listing")
 ]
